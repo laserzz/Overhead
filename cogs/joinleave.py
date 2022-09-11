bc6e127fda469c7b8b164aacf0618308f4759b36
laserzz <82984448+laserzz@users.noreply.github.com>
2022-09-10 22:35:20 -0700

    added join/leave log embeds

--- cogs/joinleave.py
+++ cogs/joinleave.py
@@ -9,6 +9,12 @@ class joinleave(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild:discord.Guild):
         user = self.bot.user
+        embed2 = discord.Embed(title="Guild Join", description=f"Joined {guild.name}\n**Member Count:** {guild.member_count}", color=0x00ff00)
+        embed2.set_thumbnail(url=guild.icon.url)
+        embed2.set_footer(text=f"ID: {guild.id}")
+        ch = self.bot.get_channel(1018377407473922099)
+        await ch.send(embed=embed2)
+
         embed = discord.Embed(title="Thanks For Inviting Me!", description="Type /help for more information.")
         embed.set_thumbnail(url=user.avatar.url)
         for channel in guild.channels:
@@ -23,7 +29,13 @@ class joinleave(commands.Cog):
         ls = [apdcoll, vcoll, apucoll, vdcoll, vmcoll, vucoll, coll]
         gid = {"_id": guild.id}
         for colls in ls:
-            await colls.delete_one(gid)  
+            await colls.delete_one(gid)
+
+        em = discord.Embed(title="Guild Leave", description=f"Left {guild}", color=0xff0000)
+        em.set_thumbnail(url=guild.icon.url)
+        em.set_footer(text=f"ID: {guild.id}")
+        ch = self.bot.get_channel(1018377407473922099)
+        await ch.send(embed=em)
 
 def setup(bot):
     bot.add_cog(joinleave(bot))
